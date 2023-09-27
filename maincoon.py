7eaf72351093633e793580c8a9852f773f4b3bb0
markus hedenborn <markus.hedenborn@triggerfish.se>
2023-09-27 14:15:10 +0200

    separates constants, refactors functions for clarity, and simplifies the color classification process using a list.

--- maincoon.py
+++ maincoon.py
@@ -4,67 +4,58 @@ import cv2
 import tensorflow as tf
 import numpy as np
 
-cat_cascade = cv2.CascadeClassifier("haarcascade_frontalcatface.xml")
-model = tf.keras.models.load_model("cat_color_model.h5")
+# Constants
+CASCADE_PATH = "haarcascade_frontalcatface.xml"
+MODEL_PATH = "cat_color_model.h5"
+COLORS = ["Black", "White", "Unknown"]
+VIDEO_SOURCE = 0  # 0 for default webcam
+RECTANGLE_COLOR = (0, 255, 0)
+FONT = cv2.FONT_HERSHEY_SIMPLEX
+FONT_SCALE = 0.5
+FONT_COLOR = (0, 255, 0)
+FONT_THICKNESS = 2
+MIN_FACE_SIZE = (50, 50)
+SCALE_FACTOR = 1.3
+MIN_NEIGHBORS = 5
+
+# Load cascade and model
+cat_cascade = cv2.CascadeClassifier(CASCADE_PATH)
+model = tf.keras.models.load_model(MODEL_PATH)
+
+def process_image(image):
+    image = cv2.resize(image, (64, 64))
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Convert to RGB
+    image = image / 255.0  # Normalize
+    image = np.expand_dims(image, axis=0)
+    return image
 
 def classify_cat_color(image):
-    image = cv2.resize(image, (64, 64))
-    
-    # Convert the image to RGB color space
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    
-    # Normalize pixel values to [0, 1]
-    image = image / 255.0
-    
-    # Add batch dimension
-    image = np.expand_dims(image, axis=0)
-
-    # Predict the color
-    predictions = model.predict(image)
+    processed_image = process_image(image)
+    predictions = model.predict(processed_image)
     color_index = np.argmax(predictions)
-    if color_index == 0:
-        return "Black"
-    elif color_index == 1:
-        return "White"
-    else:
-        return "Unknown"
+    return COLORS[color_index]
 
-# Initialize the webcam or use a video file
-cap = cv2.VideoCapture(0)  # 0 for the default webcam, or specify a video file path
+# Initialize video capture
+cap = cv2.VideoCapture(VIDEO_SOURCE)
 
 while True:
-    # Read a frame from the video source
     ret, frame = cap.read()
-
     if not ret:
         break
 
-    # Convert the frame to grayscale for face detection
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cat_faces = cat_cascade.detectMultiScale(gray, scaleFactor=SCALE_FACTOR, minNeighbors=MIN_NEIGHBORS, minSize=MIN_FACE_SIZE)
 
-    # Detect cat faces in the frame
-    cat_faces = cat_cascade.detectMultiScale(
-        gray, scaleFactor=1.3, minNeighbors=5, minSize=(50, 50)
-    )
-
-    # Classify the color of each detected cat face
     for x, y, w, h in cat_faces:
         cat_face = frame[y : y + h, x : x + w]
         color = classify_cat_color(cat_face)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), RECTANGLE_COLOR, 2)
+        cv2.putText(frame, color, (x, y - 10), FONT, FONT_SCALE, FONT_COLOR, FONT_THICKNESS)
 
-        # Draw a rectangle and label with the color
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv2.putText(
-            frame, color, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2
-        )
-
-    # Display the frame with cat faces and their colors
     cv2.imshow("CatFace Color Detection", frame)
 
-    # Exit the loop if 'q' key is pressed
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
-# Release the video source and close the OpenCV window
 cap.release()
 cv2.destroyAllWindows()
